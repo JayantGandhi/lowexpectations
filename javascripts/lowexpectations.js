83a9291a9cf8397cfecdb00efb03659a374d06a2
Jayant Gandhi <jgandhi@thrillist.com>
2014-04-11 17:30:16 -0400

    boom!

--- javascripts/lowexpectations.js
+++ javascripts/lowexpectations.js
@@ -1,3 +1,23 @@
 $(document).mousemove(function(e){
     $("#jedface").css({left:e.pageX, top:e.pageY});
 });
+
+$(document).on('ready', function(){
+	var $theTitle = $('#theTitle'),
+		$theSubtitle = $('#theSubtitle'),
+		odd = true;
+
+	setInterval(function() {
+		if (odd) {
+			$theTitle.animate({
+				'font-size': '100px'
+			}, 2000);
+		} else {
+			$theTitle.animate({
+				'font-size': '2em'
+			}, 2000);
+		}
+		odd = !odd;
+	}, 2000);
+
+});
