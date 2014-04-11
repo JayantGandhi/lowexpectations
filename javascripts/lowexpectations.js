ed489ed3e33ac89bd4b1c5b97723fe3533d90c04
Jayant Gandhi <jgandhi@thrillist.com>
2014-04-11 18:03:46 -0400

    cant click on links

--- javascripts/lowexpectations.js
+++ javascripts/lowexpectations.js
@@ -5,7 +5,17 @@ $(document).mousemove(function(e){
 $(document).on('ready', function(){
 	var $theTitle = $('#theTitle'),
 		$theSubtitle = $('#theSubtitle'),
+		$links = $('a'),
 		odd = true;
+	console.log($links);
+	$links.mouseover(function() {
+		console.log("EEEK~!");
+        var dWidth = 400, // 100 = image width
+            dHeight = 400, // 100 = image height
+            nextX = Math.floor(Math.random() * dWidth),
+            nextY = Math.floor(Math.random() * dHeight);
+        $(this).animate({ left: nextX + 'px', top: nextY + 'px' });
+    });
 
 	setInterval(function() {
 		if (odd) {
